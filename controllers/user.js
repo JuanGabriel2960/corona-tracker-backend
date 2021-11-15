464fc58b734d0ef288e8efe14c107b071dc78ee0
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2021-11-15 00:03:10 -0400

    build: added bcrypt dependencies

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,25 @@
 const { request, response } = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs')
 
 const userGet = (req = request, res = response) => {
     res.json({
         msg: 'get API'
     })
 }
-const userPost = (req = request, res = response) => {
+const userPost = async (req = request, res = response) => {
+    const {email, password, role} = req.body
+    const user = new User({email, password, role})
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync()
+    user.password = bcrypt.hashSync(password, salt)
+
+    await user.save()
+
     res.json({
-        msg: 'post API'
+        msg: 'post API',
+        user
     })
 }
 const userPut = (req = request, res = response) => {
