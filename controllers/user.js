d51aaee71eb379c2c9b6297405cc962a0df79264
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2021-11-15 00:55:25 -0400

    feat: added fiel validations config

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express');
 const User = require('../models/user');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const { validate } = require('../models/user');
 
 const userGet = (req = request, res = response) => {
     res.json({
@@ -8,8 +9,17 @@ const userGet = (req = request, res = response) => {
     })
 }
 const userPost = async (req = request, res = response) => {
+    
+
     const {email, password, role} = req.body
     const user = new User({email, password, role})
+
+    const alreadyExist = await User.findOne({email})
+    if(alreadyExist){
+        return res.status(400).json({
+            error: 'The email already exists'
+        })
+    }
 
     // Encrypt password
     const salt = bcrypt.genSaltSync()
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,29 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { userGet, userPost, userPut, userDelete } = require('../controllers/user');
+const { validateFields } = require('../middlewares/validate-fields');
+const Role = require('../models/role')
 
 const router = Router()
 
 router.get('/', userGet)
-router.post('/', userPost)
+
+router.post('/', 
+[
+    check('email', 'Email is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password is required').not().isEmpty(),
+    check('password', 'Password must contain at least 8 characters').isLength({min: 8}),
+    check('role').custom( async(role = '') => {
+        const roleExist = await Role.findOne({role})
+        if(!roleExist){
+            throw new Error(`Role ${role} is not valid`)
+        }
+    }),
+    validateFields
+],
+userPost)
+
 router.put('/', userPut)
 router.delete('/', userDelete)
 
