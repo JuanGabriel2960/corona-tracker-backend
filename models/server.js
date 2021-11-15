9a93258563d814e62ff004bebf55f76f5164ac39
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2021-11-14 23:05:35 -0400

    feat: added database connection config

--- models/server.js
+++ models/server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 
+const { dbConnection } = require('../database/config')
+
 class Server{
     constructor(){
         this.app = express()
         this.basicPathGroup = '/api/basic'
         this.userPathGroup = '/api/users'
 
+        // Connect to database
+        this.connectDB()
+
         // Middlewares
         this.middlewares()
 
         // Routes
         this.routes()
+    }
+
+    async connectDB(){
+        await dbConnection()
     }
 
     middlewares(){
